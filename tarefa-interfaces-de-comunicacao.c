790943d4aaa5da39119f6e87397eb4e0db661656
Richard <richard.esclima@gmail.com>
2025-02-08 15:35:04 -0300

    feat: adiciona configuração para uso do display

--- tarefa-interfaces-de-comunicacao.c
+++ tarefa-interfaces-de-comunicacao.c
@@ -1,14 +1,40 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
 
-
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
-        printf("Hello, world!\n");
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+    ssd1306_t ssd;
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT);
+    ssd1306_config(&ssd);
+    ssd1306_send_data(&ssd);
+
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
+    while (true)
+    {
+        ssd1306_fill(&ssd, false);
+        ssd1306_rect(&ssd, 3, 3, 122, 58, true, false);
+
+        ssd1306_draw_string(&ssd, "Aa", 8, 10);
+
+        ssd1306_send_data(&ssd);
         sleep_ms(1000);
     }
 }
